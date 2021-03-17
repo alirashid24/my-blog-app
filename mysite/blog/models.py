7337452f59c05cd655b48bf405b65c34f2b107e4
alirashid24 <ali.7860rashid@gmail.com>
2021-03-17 10:55:13 +0530

    project and blog app add

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self)\
+                .get_queryset()\
+                .filter(status='Published')
 
 class Post(models.Model):
     STATUS_CHOICES = ( 
         ('draft', 'Draft'),
          ('published', 'Published'), 
     )
-    
+    objects=models.Manager()    #the default manager
+    published=PublishedManager()#our custom manager
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
@@ -17,6 +26,12 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES,default='draft')
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                              self.publish.month,
+                              self.publish.day, self.slug])
 
     class Meta:
         ordering = ('-publish',)
